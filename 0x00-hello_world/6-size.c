02a829fa59530461778db7541216afb5dfb9eae8
kimberley-saka <kimberleysaka@gmail.com>
2022-09-10 07:53:20 -0700

    printing the memory spaces for c variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ * prints the size of memory spaces for all c variables
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
