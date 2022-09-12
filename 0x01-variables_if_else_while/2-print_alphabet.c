64954e8545e82d22e718d128904f0e02bea8e062
kimberley-saka <kimberleysaka@gmail.com>
2022-09-12 11:52:21 -0700

    printing the alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+		putchar('\n');
+	}
+
+
+	return (0);
+}
