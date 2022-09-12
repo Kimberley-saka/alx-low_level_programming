9fa74be32ab3d591d17e0484c62a7db8e7b925a6
kimberley-saka <kimberleysaka@gmail.com>
2022-09-12 13:01:31 -0700

    printing base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	int  numbr = 0;
+
+	while (numbr <= 10)
+	{	printf("%d", numbr);
+		numbr;
+	}
+
+	putchar('\n');
+	return (0);
+}
