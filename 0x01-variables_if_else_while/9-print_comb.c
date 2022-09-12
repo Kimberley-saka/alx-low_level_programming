3b3283dbe0aa06d6af3b7c6b5cd1072d51e5c800
kimberley-saka <kimberleysaka@gmail.com>
2022-09-12 14:04:32 -0700

    fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,7 +15,9 @@ int main(void)
 	{	putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		putchar(',');                                                                   putchar(' ');
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
