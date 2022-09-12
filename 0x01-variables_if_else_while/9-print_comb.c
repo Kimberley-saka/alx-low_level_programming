e85081336d9a7b89f725867143ebeba5cdba9ab8
kimberley-saka <kimberleysaka@gmail.com>
2022-09-12 13:57:33 -0700

    ossible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <time.h>
+
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{	putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');                                                                   putchar(' ');
+	}
+	return (0);
+}
