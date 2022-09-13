1abff3b5ac02903f487e9f385dff15c638550a4f
kimberley-saka <kimberleysaka@gmail.com>
2022-09-13 10:31:25 -0700

    functions and header files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{	_putchar(i);
+	}
+
+	_putchar('\n');
+	return (0);
+}
