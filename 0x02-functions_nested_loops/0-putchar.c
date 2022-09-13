79277937b94ddca4672475a595db666b2fd1ba30
kimberley-saka <kimberleysaka@gmail.com>
2022-09-13 06:22:19 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
