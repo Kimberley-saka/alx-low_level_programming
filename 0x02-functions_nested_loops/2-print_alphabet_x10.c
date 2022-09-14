ca42a19a228c91a4788861e0ad52a951ab4039d6
kimberley-saka <kimberleysaka@gmail.com>
2022-09-14 12:28:36 -0700

    fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,12 @@
  */
 void print_alphabet_x10(void)
 {	char i;
-	int j;
+	int j;	
 
 	for (j = 1; j <= 10; j++)
-		for (i = 'a'; i <= 'z'; i++)
-		{	_putchar(i);
+	{	for (i = 'a'; i <= 'z'; i++)
+		{	_putchar(i); 
 		}
 		_putchar('\n');
+	}   	
 }
