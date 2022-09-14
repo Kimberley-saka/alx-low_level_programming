396a92c3f3458a387aeb849d9c3226bc2601d1a7
kimberley-saka <kimberleysaka@gmail.com>
2022-09-14 14:30:45 -0700

    absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - calculate the absolute value of the number from zero.
+ * @j: the integer to be converted.
+ * Return: absolute value of the input number.
+ */
+int _abs(int j)
+{
+	if (j < 0)
+		j = j * -1;
+	return (j);
+}
+
