db322451dfba80e0cebfa7e305cb8dea7f8d6c00
kimberley-saka <kimberleysaka@gmail.com>
2022-09-15 12:20:42 -0700

    testing if a number is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests if number if positive or negative
+ * Return: o for success
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include<stdio.h>
+
+#include "main.h"
+/**
+ * positive_or_negative - prints positive or negative number
+ * @i: number being tested
+ * Return: Always return 0 (Success)
+ */
+void positive_or_negative(int i)
+{	i = 0;
+	if (i > 0)
+	{	printf("%d is positive\n", i);
+	}
+
+	else if (i == 0)
+	{	printf("%d is zero\n", i);
+	}
+
+	else
+	{	printf("%d is negative\n", i);
+	}
+}
