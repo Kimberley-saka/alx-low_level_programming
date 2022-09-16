c8344fb97a9b38cc57098a40b3112d06e8df1aac
kimberley-saka <kimberleysaka@gmail.com>
2022-09-16 08:49:19 -0700

    printing numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
