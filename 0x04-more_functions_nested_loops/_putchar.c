b9f33451c6b92e6043c8102a0e16892c0d76940c
kimberley-saka <kimberleysaka@gmail.com>
2022-09-18 03:44:32 -0700

    fixed

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - putchar() function
+ * @i: character to be used
+ * Return: character
+ */
+
+char _putchar(char i)
+{
+	putchar(i);
+	return (i);
+}
+
