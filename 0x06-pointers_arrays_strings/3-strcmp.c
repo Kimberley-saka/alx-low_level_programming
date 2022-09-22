6c291bd171ea659d64ca171c24fd20b4964e5e35
kimberley-saka <kimberleysaka@gmail.com>
2022-09-22 02:26:46 -0700

    compare 2 strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp _ compares two strings
+ * @s1: string 1 to be compared
+ * @s2: strong 2 to be compared
+ * Return: comp,string comparison value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int comp  = 0;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			comp = s1[i] - s2[i];
+			break;
+		}
+	}
+	return (comp);
+}
+
