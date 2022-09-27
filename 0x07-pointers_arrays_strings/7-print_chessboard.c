89fd021fcb869db1883a4f9593de464aaaced95f
kimberley-saka <kimberleysaka@gmail.com>
2022-09-27 12:35:56 -0700

    incorrect output error fixed

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,17 +8,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i = 0;
-	int j = 0;
+	unsigned int i, j;
 
-	while (i < 8)
+	for (i = 0; i < 8; i++)
 	{
-		while (j < 8)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			j++;
+			_putchar('\n');
 		}
-		_putchar('\n');
-		i++;
 	}
 }
