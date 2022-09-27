ce8ac6186e435f74b46ff5cd14ef61210802bd68
kimberley-saka <kimberleysaka@gmail.com>
2022-09-27 12:29:43 -0700

    fixed

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-char _putchar(char i);
+int _putchar(char c);
 #endif
