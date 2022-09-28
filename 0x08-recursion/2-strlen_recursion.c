dd9be7eea1e54a2d4f3447030cd9e64eba63e1ed
kimberley-saka <kimberleysaka@gmail.com>
2022-09-28 11:14:26 -0700

    fixed

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,9 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\n')
+	if (*s == '\0')
 	{
 		return (0);
 	}
-	s++;
-	return (1 + _strlen_recursion(s));
+	return (1 + _strlen_recursion(s + 1));
 }
