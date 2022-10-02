9fb8ebd0ad7494d6be64682f46f5e42305a10c2c
kimberley-saka <kimberleysaka@gmail.com>
2022-10-02 10:18:34 -0700

    multiply 2 numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies 2 numbers
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: zero for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	if (argc != 3)
+	{
+		puts("Error");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+	printf("%d\n", c);
+	return (0);
+}
