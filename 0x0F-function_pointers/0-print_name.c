f9bcc7091f5f8b6a2af9e0931243b8892ff68399
kimberley-saka <kimberleysaka@gmail.com>
2022-10-17 09:17:53 -0700

    fixed error

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,12 @@
 /**
  * print_name - prints a name
  * @name: name to print
- * f: function to print with
+ * @f: function to print with
  * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-		f(name);
+		(*f)(name);
 }
