ae096bdb5e0c14d528f84ac470aa0c91c2aeb27d
kimberley-saka <kimberleysaka@gmail.com>
2022-10-17 10:33:58 -0700

    error fixed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name
@@ -10,5 +10,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-		(*f)(name);
+		f(name);
 }
