5cabc57d714ce90b1a261850182b30b17445dbed
kimberley-saka <kimberleysaka@gmail.com>
2022-10-29 12:03:07 -0700

    prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,12 @@
  * print_name - prints a name
  * @name: name to print
  * @f: function pointer
+ * Return : nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		(*f)(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
