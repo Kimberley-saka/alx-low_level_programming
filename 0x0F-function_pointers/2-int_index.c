e90a3ede659841edbbfcf5245710565fe44318f0
kimberley-saka <kimberleysaka@gmail.com>
2022-10-30 10:25:49 -0700

    bug fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,10 +17,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-		for (i = 0; i < cmp; i++)
+		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
+	return (-1);
 }
