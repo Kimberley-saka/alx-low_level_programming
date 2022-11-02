5b49427a77b2c205e90bb9d04291b07cc95ddf81
kimberley-saka <kimberleysaka@gmail.com>
2022-11-02 13:28:15 -0700

    singly linked list continued

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - rints all the elements of a listint_t list
+ * @h: pointer to structure
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->next);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains code to do with linked lists; a data structure that uses nodes to store data. These nodes contain links i.e memory addresses ponting to the next node
