804ccd521c3341532b0aaab73976a945b9d20bd5
kimberley-saka <kimberleysaka@gmail.com>
2022-11-07 10:01:07 -0800

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function with one argument
+ * @head: pointer to head
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		node = *head->next;
+		free(*head);
+		*head = node;
+	}
+	*head = NULL;
+}
