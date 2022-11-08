88a55f19b363916b4eef32d0679f571054c91050
kimberley-saka <kimberleysaka@gmail.com>
2022-11-08 10:16:41 -0800

    betty error fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,18 +5,16 @@
  * @head: pointer to a pointer
  * @idx: index of the list where the new node should be added
  * @n: value to set new node to
- * @Return: the address of the new node, or NULL if it failed
+ * Return: the address of the new node, or NULL if it failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *cursor;
 	unsigned int count = 0;
-	
+
 	if (head == NULL)
-	{
 		return (NULL);
-	}
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
 		return (NULL);
